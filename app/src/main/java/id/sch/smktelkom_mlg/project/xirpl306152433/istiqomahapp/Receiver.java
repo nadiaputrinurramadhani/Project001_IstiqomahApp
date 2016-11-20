fb1c25469c9e0babdc6a682ff1442bbbc5611eca
Dayinta-PC <dayintaindahsari@gmail.com>
2016-11-20 23:12:59 +0700

    raw commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl306152433/istiqomahapp/Receiver.java
@@ -0,0 +1,23 @@
+package id.sch.smktelkom_mlg.project.xirpl306152433.istiqomahapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+/**
+ * Created by Dayinta-PC on 11/20/2016.
+ */
+
+public class Receiver extends BroadcastReceiver {
+    MediaPlayer mp;
+
+    @Override
+    public void onReceive(Context c, Intent arg1) {
+        mp = MediaPlayer.create(c, R.raw.song);
+        mp.start();
+        Toast.makeText(c, "Alarm Telah Menyala", Toast.LENGTH_SHORT).show();
+    }
+}
+
