0a81f8cce75a900112128162686b964e1204886e
Sella N P <sella13300@gmail.com>
2016-11-30 13:20:32 +0700

    Commit awal

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl306152433/istiqomahapp/Qiblat.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl306152433.istiqomahapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Qiblat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qiblat);
+    }
+}
